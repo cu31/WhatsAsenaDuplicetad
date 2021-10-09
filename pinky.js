10c927a2a37d04e85274cacd2f141f4c329d24cc
cu31 <91752009+cu31@users.noreply.github.com>
2021-10-09 14:16:54 +0300

    Update pinky.js

--- pinky.js
+++ pinky.js
@@ -11,12 +11,12 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
- OB_NAME: process.env.Z_BOT_NAME === undefined ? 'Pinky' : process.env.Z_BOT_NAME,
- OA_NAME: process.env.Z_DEP_NAME === undefined ? 'plk' : process.env.Z_DEP_NAME,
+ OB_NAME: process.env.Z_BOT_NAME === undefined ? 'Darks' : process.env.Z_BOT_NAME,
+ OA_NAME: process.env.Z_DEP_NAME === undefined ? 'drk' : process.env.Z_DEP_NAME,
  REMOVE2: process.env.THERI_KICK_PM === undefined ? 'false' : process.env.THERI_KICK_PM,
  PLKAFN: process.env.THERI_LIST_PM === undefined ? false : process.env.THERI_LIST_PM,
  OA_REPLY: process.env.Z_DEP_REPLY === undefined ? 'you typed plk , he is my creator' : process.env.Z_DEP_REPLY,
- PHONE: process.env.NUMBER === undefined ? '+918301985859' : process.env.NUMBER,   
+ PHONE: process.env.NUMBER === undefined ? '+5510310485' : process.env.NUMBER,   
     
 
 };
